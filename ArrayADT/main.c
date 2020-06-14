e9c632c02b3e937f11be6887b82076fff51b83ae
Subhasish <subhasish469@gmail.com>
2020-06-14 12:49:23 +0530

    reversing using auxillary array and using swap both

--- ArrayADT/main.c
+++ ArrayADT/main.c
@@ -163,6 +163,36 @@ void avg(struct Array arr){
 
 }
 
+void reverse(struct Array arr){
+    int B[arr.length];
+    int i,j;
+    for(i=arr.length-1,j=0;i>=0,j<arr.length;i--,j++){
+        B[j]=arr.A[i];
+    }
+    for(i=0;i<arr.length;i++){
+            arr.A[i]=B[i];
+
+    }
+    printf("\nprinting the reversed array\n");
+    for(int i=0;i<arr.length;i++){
+        printf("%d ",arr.A[i]);
+    }
+
+}
+void reverseSwap(struct Array arr){
+    int i,j,temp;
+    for(i=0,j=arr.length-1;i<arr.length,j>=0;i++,j--){
+        temp=arr.A[i];
+        arr.A[i]=arr.A[j];
+        arr.A[j]=temp;
+    }
+        printf("\nprinting the reversed array using swap\n");
+        for(int i=0;i<arr.length;i++){
+            printf("%d ",arr.A[i]);
+    }
+
+}
+
 
 
 
@@ -230,6 +260,8 @@ int main()
         min(arr);
         sum(arr);
         avg(arr);
+        reverse(arr);
+        reverseSwap(arr);
 
 
 
