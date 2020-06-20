6040c596a6271a1c9f90d4701e979dcf9e207707
Subhasish <subhasish469@gmail.com>
2020-06-20 20:53:42 +0530

    adding sparse matrices

--- sparseMatrix/main.c
+++ sparseMatrix/main.c
@@ -38,21 +38,81 @@ void display(struct sparseMatrix sm){
     for(i=1;i<=sm.m;i++){
         for(j=1;j<=sm.n;j++){
             if(i==sm.e[k].i && j==sm.e[k].j){
-                printf("%d ",sm.e[k].x);
+                printf("%d\t",sm.e[k].x);
                 k++;
 
             }else{
-                printf("%d ",0);
+                printf("%d\t",0);
             }
         }
         printf("\n");
     }
 }
+struct sparseMatrix * add(struct sparseMatrix *sm1,struct sparseMatrix *sm2){
+    int i,j,k;
+    i=j=k=0;
+    struct sparseMatrix *sum;
+    sum=(struct sparseMatrix*)malloc(sizeof(struct sparseMatrix));
+    sum->e=(struct elements*)malloc(sm1->numOfNonZeros+sm2->numOfNonZeros*sizeof(struct elements));
+    while(i<sm1->numOfNonZeros&&j<sm2->numOfNonZeros){
+        if(sm1->e[i].i<sm2->e[j].i){
+            sum->e[k]=sm1->e[i];
+            i++;
+            k++;
+        }else if(sm1->e[i].i>sm2->e[j].i){
+            sum->e[k]=sm2->e[j];
+            j++;
+            k++;
+        }else{
+            if(sm1->e[i].j<sm2->e[j].j){
+                sum->e[k]=sm1->e[i];
+                i++;
+                k++;
+            }else if(sm1->e[i].j>sm2->e[j].j){
+                sum->e[k]=sm2->e[j];
+                j++;
+                k++;
+            }else{
+                sum->e[k]=sm1->e[i];
+                sum->e[k].x=sm1->e[i].x+sm2->e[j].x;
+                k++;
+                i++;
+                j++;
+
+            }
+
+        }
+
+    }
+    for(;i<sm1->numOfNonZeros;i++){
+        sum->e[k]=sm1->e[i];
+        k++;
+    }
+    for(;j<sm1->numOfNonZeros;j++){
+        sum->e[k]=sm2->e[j];
+        k++;
+    }
+    sum->m=sm1->m;
+    sum->n=sm1->n;
+    sum->numOfNonZeros=k;
+
+    return sum;
+
+
+
+}
 int main()
 {
-    struct sparseMatrix sm;
-    create(&sm);
-    display(sm);
+    struct sparseMatrix sm1,sm2,*sm3;
+    create(&sm1);
+    create(&sm2);
+    sm3=add(&sm1,&sm2);
+    printf("Matrix 1\n");
+    display(sm1);
+    printf("Matrix 2\n");
+    display(sm2);
+    printf("Matrix sum\n");
+    display(*sm3);
 
     return 0;
 }
