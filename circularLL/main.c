95800c4e48b22592f1dcad7df2b62e07527e1ce8
Subhasish <subhasish469@gmail.com>
2020-06-26 10:59:32 +0530

    circular linked list insertion

--- circularLL/main.c
+++ circularLL/main.c
@@ -26,10 +26,45 @@ void display(struct Node *h){
         h=h->next;
     }while(h!=Head);
 }
+void insertElement(struct Node *h,int pos,int data){
+    struct Node *t=NULL;
+    int i;
+    if(pos==0){
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=data;
+        if(Head==NULL){
+            Head=t;
+            Head->next=Head;
+        }else{
+            while(h->next!=Head){
+                h=h->next;
+            }
+            h->next=t;
+            t->next=Head;
+            Head=t;
+        }
+
+
+    }else{
+        for(i=0;i<pos-1;i++){
+            h=h->next;
+        }
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=data;
+        t->next=h->next;
+        h->next=t;
+    }
+}
 int main()
 {
     int A[]={2,3,4,5,6};
     create(A,5);
+    display(Head);
+    insertElement(Head,0,299);
+    printf("\n");
+    display(Head);
+    insertElement(Head,3,599);
+    printf("\n");
     display(Head);
     return 0;
 }
