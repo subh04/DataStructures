cc1694111aa7332307c38db374a7db754473fade
Subhasish <subhasish469@gmail.com>
2020-07-07 12:44:30 +0530

    Binary search creation recursively

--- BSTcreationAndInsertion/main.c
+++ BSTcreationAndInsertion/main.c
@@ -4,7 +4,7 @@ struct Node{
     struct Node *lchild;
     int data;
     struct Node *rchild;
-}*root=NULL;
+}*root=NULL,*rootNew=NULL;
 void insert(int key){
     struct Node *p,*t=root,*q;
     if(root==NULL){
@@ -61,19 +61,49 @@ void search(struct Node *p,int key){
     }
     printf("\nkey not found\n");
 }
+struct Node * rInsert(struct Node * p,int key){
+    struct Node *t=NULL;
+    if(p==NULL){
+        t=(struct Node *)malloc(sizeof(struct Node));
+        t->data=key;
+        t->lchild=t->rchild=NULL;
+        return t;
+    }
+    if(key<p->data)
+        p->lchild=rInsert(p->lchild,key);
+    else if(key>p->data)
+        p->rchild=rInsert(p->rchild,key);
+
+    return;
+};
 int main()
 {
+    insert(30);
     insert(20);
-    insert(32);
-    insert(2);
-    insert(89);
-    insert(54);
-    insert(-2);
+    insert(40);
+    insert(10);
+    insert(25);
+    insert(35);
+    insert(45);
+    insert(42);
+    insert(43);
     inorder(root);
     //printf("\n");
     search(root,-2);
     search(root,20);
     search(root,111);
     //printf("Hello world!\n");
+    rootNew=rInsert(rootNew,30);
+    rInsert(rootNew,20);
+    rInsert(rootNew,40);
+    rInsert(rootNew,10);
+    rInsert(rootNew,25);
+    rInsert(rootNew,35);
+    rInsert(rootNew,45);
+    rInsert(rootNew,42);
+    rInsert(rootNew,43);
+    inorder(rootNew);
+    search(root,20);
+    //Insert(rootNew,30);
     return 0;
 }
