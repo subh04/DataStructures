23b1aae01faf88558fa571321f8894f7c6190a2b
Subhasish <subhasish469@gmail.com>
2020-07-10 12:25:10 +0530

    HEAP DELETION

--- heap/main.c
+++ heap/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void delete(int A[],int n){
+    int x,i,j;
+    //x=A[n];
+    A[1]=A[n];
+    i=1;
+    j=2*i;
+    while(j<n-1){
+        if(A[j+1]>A[j])
+            j=j+1;
+        if(A[i]<A[j]){
+            x=A[j];
+            A[j]=A[i];
+            A[i]=x;
+            i=j;
+            j=2*i;
+        }else{
+            break;
+        }
+    }
+}
 void insert(int A[],int n){
     int i=n,temp;
     temp=A[i];
@@ -13,19 +33,28 @@ void insert(int A[],int n){
 
 
 
-    for(i=0;i<=7;i++){
-        printf("%d ",A[i]);
-    }
+
 
 
 }
 void create(){
-    int A[]={0,10,20,30,25,5,40,35};
+    int A[]={0,40,35,30,15,10,2,5};
     int i;
     for(i=2;i<=7;i++){
         insert(A,i);
-        printf("\n");
+
     }
+    print(A,7);
+    delete(A,7);
+    print(A,6);
+
+}
+void print(int A[],int n){
+int i;
+for(i=1;i<=n;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
 }
 int main()
 {
